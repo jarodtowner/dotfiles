a2da7d190b2928ae9d3638dca25a977ced59862e
Jarod Towner <jarod.towner@gmail.com>
2020-11-27 15:21:26 +1030

    Changed some file locations to match standards

--- zsh/plugins/scripts.sh
+++ zsh/plugins/scripts.sh
@@ -1,11 +1,12 @@
 function scripts() {
   if [ -f package.json ]; then
     SCRIPT=$(cat package.json | jq '.scripts | keys | .[]' -r | fzf)
-    echo "> npm run $SCRIPT"
-    npm run $SCRIPT
+    COMMAND="npm run $SCRIPT"
   elif [ -f Makefile ]; then
     SCRIPT=$(cat Makefile | grep -e "^[a-zA-Z0-9]*:" | sed "s/:.*//g" | fzf)
-    echo "> make $SCRIPT"
-    make $SCRIPT
+    COMMAND="npm run $SCRIPT"
   fi
+  echo "> $COMMAND"
+  print -s $COMMAND
+  eval $COMMAND
 }
