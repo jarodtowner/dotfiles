3dbf6cbf88431111866991b005b5fcaaf0f57a30
Jarod Towner <jarod.towner@gmail.com>
2020-11-30 13:31:30 +1030

    Added yarn detection to scripts command

--- zsh/autoload/scripts.sh
+++ zsh/autoload/scripts.sh
@@ -1,12 +1,20 @@
 function scripts() {
   if [ -f package.json ]; then
     SCRIPT=$(cat package.json | jq '.scripts | keys | .[]' -r | fzf)
-    COMMAND="npm run $SCRIPT"
+    if [ -f yarn.lock ]; then
+      COMMAND="yarn run $SCRIPT"
+    else
+      COMMAND="npm run $SCRIPT"
+    fi
   elif [ -f Makefile ]; then
     SCRIPT=$(cat Makefile | grep -e "^[a-zA-Z0-9]*:" | sed "s/:.*//g" | fzf)
     COMMAND="npm run $SCRIPT"
   fi
-  echo "> $COMMAND"
-  print -s $COMMAND
-  eval $COMMAND
+  if [ $SCRIPT ]; then
+    echo "> $COMMAND"
+    print -s $COMMAND
+    eval $COMMAND
+  else
+    echo "No script selected. Exited."
+  fi
 }
