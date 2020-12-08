5c29df41d5d5cb84c7bb03c190844b1b7eed7e8a
Jarod Towner <jarod.towner@gmail.com>
2020-12-08 17:20:15 +1030

    Tidied up package installation

---
+++ dependencies/homebrew.zsh
@@ -0,0 +1,23 @@
+if [ ! $(command -v brew) ]; then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+fi
+
+
+PACKAGES=(
+  "tmux"
+  "neovim"
+  "the_silver_searcher"
+  "node"
+  "cowsay"
+  "python3"
+  "fd"
+  "ruby"
+  "yarn"
+  "jq"
+  "ripgrep"
+  "ccls"
+)
+
+for p in $PACKAGES; do
+  brew list $p &> /dev/null || brew install $p
+done
