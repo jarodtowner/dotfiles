60445a8cc33d679d73cb6da22e8f4756dcbb328c
Ashley Towner <ashley.towner@protonmail.com>
2021-05-04 10:41:02 +0930

    Improved notes command to be able to create custom note files

--- zsh/autoload/notes.zsh
+++ zsh/autoload/notes.zsh
@@ -1,7 +1,17 @@
 function note() {
-  notefile="$XDG_DATA_HOME/notes/Note-$(date +%F).md"
-  if [ ! -f $notefile ]; then
-    echo "# $(date +"%a %d %b %Y")" > $notefile
+  if [ $1 ]
+  then
+    notefile=$XDG_DATA_HOME/notes/$1
+    mkdir -p "${notefile%/*}" && touch "$notefile"
+  else
+    notefile="$XDG_DATA_HOME/notes/Note-$(date +%F).md"
+    if [ ! -f $notefile ]; then
+      echo "# $(date +"%a %d %b %Y")" > $notefile
+    fi
   fi
-  nvim -c "norm G" $notefile
+  nvim -c "norm G" $notefile "+cd ${XDG_DATA_HOME}/notes"
+}
+
+function notes() {
+  nvim $XDG_DATA_HOME/notes "+cd ${XDG_DATA_HOME}/notes"
 }
